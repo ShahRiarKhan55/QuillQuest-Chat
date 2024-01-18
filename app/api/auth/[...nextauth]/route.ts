5bbbb76840ce9e1bec1bd4b7f5513b03c0877bff
ShahRiarKhan55 <siaabid1234@gmail.com>
2024-01-18 21:46:16 +0600

    updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -61,4 +61,5 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+export default handler;
